4c2447d87bf801dd24effc1be38325b007a40877
Miguel Yann <tsambamiguelito@gmail.com>
2020-04-24 12:09:06 +0200

    FIX encode dart object

--- lib/data/provider/user_provider.dart
+++ lib/data/provider/user_provider.dart
@@ -1,24 +1,24 @@
-import 'dart:ffi';
+import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class UserProvider {
   Dio _dio = Dio();
-  static const DEFAULT_URL = 'http://localhost:8080';
+  static const DEFAULT_URL = 'http://192.168.1.50:8080';
 
-//  Future<dynamic> logUserWithDio(String anUsername, String aPassword) async {
-//    var response = await _dio.post('$DEFAULT_URL/login',
-//        data: {'username': anUsername, 'password': aPassword});
-//    print(response.statusCode);
-//    return response;
-//  }
+  Future<dynamic> logUserWithDio(String anUsername, String aPassword) async {
+    var response = await _dio.post('$DEFAULT_URL/login',
+        data: {'username': anUsername, 'password': aPassword});
+    print(response.statusCode);
+    return response;
+  }
 
   Future<dynamic> logUserWithHttp(String aUsername, String aPassword) async {
     try {
       var response = await http.post('$DEFAULT_URL/login',
-          body: {'username': aUsername, 'password': aPassword});
-      print(response.body);
+          body: jsonEncode({'username': aUsername, 'password': aPassword}));
+      print(response.statusCode);
       return response;
     } catch (Exception) {
       print('error');
